ea3b8073e35d25f194af16c751c62e410393a57b
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-20 02:39:56 +0600

    Update

--- polls/tests/test_views/test_quizz_list_view.py
+++ polls/tests/test_views/test_quizz_list_view.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from polls.views import QuizzListAPIView
 from polls.serializers import QuizzSerializer
+
+
 # from polls.models import Quizz
 # from django.utils import timezone
 
@@ -11,3 +13,7 @@ class TestQuizListAPIView(TestCase):
 
     def test_attr_serializer_class(self):
         self.assertEqual(self.view_class.serializer_class, QuizzSerializer)
+
+    def test_get_queryset_method(self):
+        # TODO: Write test for get_queryset() method
+        pass
