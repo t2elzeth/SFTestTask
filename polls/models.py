5a1f3aa11fe692df643de924bde25a096e34f3cd
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-19 17:34:51 +0600

    Update

--- polls/models.py
+++ polls/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 
 class Question(models.Model):
-    pass
+    title = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    finish_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- polls/tests/test_models/test_question_model.py
+++ polls/tests/test_models/test_question_model.py
@@ -1,13 +1,44 @@
 from django.test import TestCase
 
 from polls.models import Question
+from django.db import models
 
 
-class TestQuestionModel(TestCase):
-    def setUp(self) -> None:
-        pass
+class TestQuestionModelFields(TestCase):
+    def get_field(self, field_name: str) -> models.Field:
+        return Question._meta.get_field(field_name)
 
     def test_field_title(self):
-        field = Question._meta.fields['title']
+        field = self.get_field("title")
 
+        self.assertIsInstance(field, models.CharField)
         self.assertEqual(field.max_length, 255)
+
+    def test_field_start_date(self):
+        field = self.get_field("start_date")
+
+        self.assertIsInstance(field, models.DateTimeField)
+
+    def test_field_finish_date(self):
+        field = self.get_field("finish_date")
+
+        self.assertIsInstance(field, models.DateTimeField)
+        self.assertTrue(field.auto_now_add)
+
+    def test_field_description(self):
+        field = self.get_field("description")
+
+        self.assertIsInstance(field, models.TextField)
+
+
+class TestQuestionModelMethods(TestCase):
+    def setUp(self) -> None:
+        self.instance = Question.objects.create(
+            title="My title",
+            start_date="2003-11-22 01:01:58",
+            description="My question description"
+        )
+
+    def test_str(self):
+        string_representation = str(self.instance)
+        self.assertEqual(string_representation, "My title")
