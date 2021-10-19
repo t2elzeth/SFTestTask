e056ea56ee43008dc6ee854bbf24f3bb5cabc584
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-20 01:34:39 +0600

    Update

---
+++ polls/tests/test_admin/test_inlines.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+from polls.admin import QuestionInline, QuestionChoiceInline
+from polls.models import Question, QuestionChoice
+
+
+class TestQuestionInline(TestCase):
+    def setUp(self) -> None:
+        self.inline_class = QuestionInline
+
+    def test_field_model(self):
+        self.assertEqual(self.inline_class.model, Question)
+
+    def test_field_extra(self):
+        self.assertEqual(self.inline_class.extra, 0)
+
+    def test_field_show_change_link(self):
+        self.assertTrue(self.inline_class.show_change_link)
+
+
+class TestQuestionChoiceInline(TestCase):
+    def setUp(self) -> None:
+        self.inline_class = QuestionChoiceInline
+
+    def test_field_model(self):
+        self.assertEqual(self.inline_class.model, QuestionChoice)
+
+    def test_field_extra(self):
+        self.assertEqual(self.inline_class.extra, 0)
+
+    def test_field_show_change_link(self):
+        self.assertTrue(self.inline_class.show_change_link)
